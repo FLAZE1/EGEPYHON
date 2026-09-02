95d9e501a1419132a864c2012af3fed190a4f7c4
Иванов Михаил <flaze@fs.loc>
2026-09-02 19:44:52 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,65 @@
+#int - целочисленный тип данных
+#str - строчный тип данных
+# s = [1,2,"a",[1,2,3,4]]
+#list - список элементов не связанных по типу
+# a=0.5
+#type - выводит тип данных
+#float - дробный тип данных
+#bool - 0/1 False/True
+#s={"key":"value"}
+#print(type(s))
+#dict - словарь набор (элементов ключ - значение)
+# s={1,2,3,4,5,4,6,3,4,2}
+# set - набор уникальных элементов
+
+#tuple - набор неизменяемых элеменов
+# s=(1,2,3,4,5)
+
+# float > int > bool
+# a = True
+# b = 5
+# result = a + b
+# print(result)
+
+# s = int(value)
+# print(int("10.8"))
+# print(int("10"))
+# print(int("10.3"))
+
+# print(float("10.3"))
+
+# a=int(float("10.2"))
+# print(a)
+
+# a=str(10)
+# print(a,type(a))
+# a=list("hello")
+# a=list((1,2,3,4))
+# print(a)
+
+# text = "Python"
+# print(list(text))
+
+# a=int(input())
+# b=float(input())
+# c=input()
+# print(a,',',b,',',c)
+
+# nums = [1, 2, 3, 4, 5]
+# print(set(nums))
+
+# product = {"название": "Ноутбук", "цена": 55000, "в наличии": True}
+# print(product["название"],"стоит",product["цена"],product["в наличии"])
+
+# coords=(55,75,10)
+# labels=["широта","долгота",["высота"]]
+# print(coords[0],labels[0],coords[1],labels[1],sep=";")
+
+# print("hello" + "world") #конкатенация
+# print(set[1,2,3]+[3,4,5])
+
+# abs() - модуль
+
+#** - степень
+#** 1/2 корень
+#(),**,* / ,+ -
